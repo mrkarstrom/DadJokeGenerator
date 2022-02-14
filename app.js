fa623ee5abb4318d82c905e458d33379a51a2762
Magnus Karström <magnus@yesweb.se>
2022-02-14 22:26:07 +0100

    working on app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  fetch('https://icanhazdadjoke.com/', config);
+}
